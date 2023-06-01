177e11f9faf3ac51b5f090b52c9ffb77033ecc1e
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-01 11:47:52 -0400

    add js to questions

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,32 @@
-//using selectors inside the element
-// traversing the dom
+//using selectors inside the element=====================
+//target elements
+const questions = document.querySelectorAll('.question');
+
+//loop over them
+questions.forEach(function (question) {
+  //   console.log(question);
+  //targeting within element not whole document
+  const btn = question.querySelector('.question-btn');
+  //   console.log(btn);
+  btn.addEventListener('click', function () {
+    //functinlaity to close other questions
+    questions.forEach(function (item) {
+      if (item != question) {
+        item.classList.remove('show-text');
+      }
+    });
+    question.classList.toggle('show-text');
+  });
+});
+
+// // traversing the dom====================================
+// //select target elements
+// const btns = document.querySelectorAll('.question-btn');
+
+// //loop over all btns and event listen, using event object, and traversing dom
+// btns.forEach(function (btn) {
+//   btn.addEventListener('click', function (e) {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle('show-text');
+//   });
+// });
