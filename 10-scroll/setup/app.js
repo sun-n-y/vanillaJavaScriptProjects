b265e72a50a80e852709b41943fbe70f392b4bd7
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-03 18:49:18 -0400

    add dynamic links height js to scroll lesson

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -7,9 +7,25 @@
 //target element
 const date = document.querySelector('#date');
 
+//method
 date.innerHTML = new Date().getFullYear();
 
 // ********** close links ************
+//target elements
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+//event listener
+navToggle.addEventListener('click', function () {
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
 
 // ********** fixed navbar ************
 
