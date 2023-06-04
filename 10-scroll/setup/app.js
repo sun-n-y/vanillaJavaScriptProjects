b1dcd27dc6e7f307b90d06ba09a6623ef7afcb01
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-03 20:01:36 -0400

    add smooth scroll js to scroll lesson

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -28,6 +28,56 @@ navToggle.addEventListener('click', function () {
 });
 
 // ********** fixed navbar ************
+//target elements
+const navbar = document.querySelector('#nav');
+const topLink = document.querySelector('.top-link');
+
+//event listeners
+window.addEventListener('scroll', function () {
+  const scrollHeight = window.scrollY;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add('fixed-nav');
+  } else {
+    navbar.classList.remove('fixed-nav');
+  }
+  if (scrollHeight > 500) {
+    topLink.classList.add('show-link');
+  } else {
+    topLink.classList.remove('show-link');
+  }
+});
 
 // ********** smooth scroll ************
-// select links
+//target elements
+const scrollLinks = document.querySelectorAll('.scroll-link');
+
+//event listener
+scrollLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    //prevent default link travel
+    e.preventDefault();
+    //navigate to specific spot
+    const id = e.currentTarget.getAttribute('href').slice(1);
+    const element = document.getElementById(id);
+    //calculate the heights
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains('fixed-nav');
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    linksContainer.style.height = 0;
+  });
+});
