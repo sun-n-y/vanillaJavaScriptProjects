8fa0eee8152490568db252bf1f3327a8ab7f1f07
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-07 11:39:31 -0400

    add display alert

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -1,10 +1,47 @@
 // ****** SELECT ITEMS **********
 
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
+
 // edit option
 
+let editElement;
+let editFLag = false;
+let editID = '';
+
 // ****** EVENT LISTENERS **********
+//submitForm
+form.addEventListener('submit', addItem);
 
 // ****** FUNCTIONS **********
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
+  if (value && !editFLag) {
+    console.log('add item to the list');
+  } else if (value && editFLag) {
+    console.log('editing');
+  } else {
+    displayAlert('empty value', 'danger');
+  }
+}
+
+//display alert
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+  //remove alert
+  setTimeout(function () {
+    alert.textContent = '';
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
+}
 
 // ****** LOCAL STORAGE **********
 
